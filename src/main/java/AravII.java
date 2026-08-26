e440b9a5d6d28e68d11e99d16f7d2baff110e4d4
Arav <aravcabral31@gmail.com>
2026-08-27 00:10:28 +0800

    Add mark as done

--- src/main/java/AravII.java
+++ src/main/java/AravII.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Scanner;
 
 public class AravII {
@@ -17,6 +19,7 @@ public class AravII {
         System.out.println(banner);
 
         List<String> tasks = new ArrayList<>();
+        Set<Integer> completedTasks = new HashSet<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
@@ -24,10 +27,17 @@ public class AravII {
                 break;
             } else if (input.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
+                    String status = completedTasks.contains(i) ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + ". " + status + " " + tasks.get(i));
                 }
             } else if (input.startsWith("todo ")) {
                 tasks.add(input.substring(5));
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                completedTasks.add(taskIndex);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                completedTasks.remove(taskIndex);
             } else {
                 System.out.println(input);
             }
