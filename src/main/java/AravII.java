2379f50dd830aa18aa30306800f3a434767c9eb7
Arav <aravcabral31@gmail.com>
2026-08-26 23:51:12 +0800

    Level 1 implemented

--- src/main/java/AravII.java
+++ src/main/java/AravII.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AravII {
     public static void main(String[] args) {
         String banner = "____________________________________________________________\n"
@@ -11,6 +13,16 @@ public class AravII {
                 + "What can I do for you?\n"
                 + "____________________________________________________________";
         System.out.println(banner);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
